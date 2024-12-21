75ab24ca897aea4fae1c1d250f62c67beb6d7ec2
ticlekiwi <florian.nicolas@yahoo.fr>
2024-12-21 15:54:44 +0100

    updating desktop app

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -4,10 +4,25 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[cfg(desktop)]
+mod tray;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_positioner::init())
+        .setup(move |app| {
+            #[cfg(target_os = "macos")]
+            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+            #[cfg(all(desktop, not(test)))]
+            {
+              let handle = app.handle();
+              tray::create_tray(handle)?;
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
