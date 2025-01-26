1b389c6e4078dfab621cc8b5f8fd3748690db946
ticlekiwi <florian.nicolas@yahoo.fr>
2025-01-26 18:25:12 +0100

    Adding Vercel Analytics

--- next-client/src/app/layout.tsx
+++ next-client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles/globals.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
+        <Analytics />
         {children}
         <Toaster />
       </body>
